0e5aa1ec4e1b02f58c4e2ba9780168b74bf71be0
yabbes <yabbaspost@gmail.com>
2016-07-05 19:30:53 +0200

    class

--- turtlearmee.py
+++ turtlearmee.py
@@ -8,15 +8,24 @@ class Kroete(turtle.Turtle):
         self.x_pos = x
         self.y_pos = y
 
+    def simpleMove(length, angle):
+        self.forward(length)
+        self.left(angle)
+        self.forward(length)
+        self.left(angle)
+
 def main():
     wn = turtle.Screen()
-    wn.bgcolor("grey")
+    wn.bgcolor("green")
 
     troop = list()
 
-    for i in range(3):
-       kr = Kroete(20, 3)
+    for i in range(4):
+       kr = Kroete(20+(i*2), 3)
        troop.append(kr)
+
+    for k in kr:
+        k.simpleMove(90, 45)
 
     wn.exitonclick()
 
