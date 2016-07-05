8496a28a6ca68b418978157cb15723fd36a39421
yabbes <yabbaspost@gmail.com>
2016-07-05 21:22:53 +0200

    with colors

--- olympic.py
+++ olympic.py
@@ -8,24 +8,26 @@ class OlyTurtle(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self, shape="turtle")
 
-    def drawCircle(self, x, y, radius=50):
+    def drawCircle(self, x, y, color, radius=60):
         self.penup()
         self.setposition(x, y)
         self.pendown()
         self.pensize(4)
+        self.color(color)
         self.circle(radius)
 
 
     def drawOlympicSymbol(self):
         """Iterates over positions predefined
         in the array """
-        positions = [(0, 0), (-120, 0), (60, 60),
-                    (-60, 60), (-180, 60)]
-        for position in positions:
-            self.drawCircle(position[0], position[1])
+        positions = [(0, 0, "blue"), (-120, 0, "purple"), (60, 60, "red"),
+                    (-60, 60, "yellow"), (-180, 60, "green")]
+        for x, y, color in positions:
+            self.drawCircle(x, y, color)
 
 if __name__ == '__main__':
     wn = turtle.Screen()
+    wn.bgcolor("lightgrey")
     t = OlyTurtle()
     t.drawOlympicSymbol()
     wn.exitonclick()
